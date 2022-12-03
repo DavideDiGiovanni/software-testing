00c08998e8ea244a36ef981cf378460f420816e5
Davide Di Giovanni <davidedigio@yahoo.it>
2022-12-03 19:46:02 +0100

    Added initial classes.

---
+++ src/main/java/com/amigoscode/testing/customer/CustomerRegistrationService.java
@@ -0,0 +1,36 @@
+package com.amigoscode.testing.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomerRegistrationService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerRegistrationService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public void registerNewCustomer(CustomerRegistrationRequest request) {
+        Customer requestCustomer = request.getCustomer();
+        String requestPhoneNumber = requestCustomer.getPhoneNumber();
+        Optional<Customer> optionalCustomer = customerRepository.selectCustomerByPhoneNumber(requestPhoneNumber);
+
+        if (optionalCustomer.isEmpty()) {
+            customerRepository.save(requestCustomer);
+            return;
+        }
+
+        Customer customer = optionalCustomer.get();
+
+        if(requestCustomer.getName().equals(customer.getName())) {
+            return;
+        }
+
+        throw new IllegalStateException(String.format("Phone number [%s] already taken", requestPhoneNumber));
+    }
+}
