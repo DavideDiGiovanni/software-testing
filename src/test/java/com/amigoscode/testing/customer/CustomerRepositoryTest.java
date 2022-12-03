a9bf88e788d698b3e08cb992b56096d99ad5bb59
Davide Di Giovanni <davidedigio@yahoo.it>
2022-12-03 19:50:29 +0100

    Added sql test with Assertions.

---
+++ src/test/java/com/amigoscode/testing/customer/CustomerRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.amigoscode.testing.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository underTest;
+
+    @Test
+    void itShouldSaveCustomer() {
+        // Given
+        UUID id = UUID.randomUUID();
+        String name = "Dave";
+        String phoneNumber = "0123456789";
+        Customer customer = new Customer(id, name, phoneNumber);
+
+        // When
+        underTest.save(customer);
+
+        // Then
+        Optional<Customer> optionalCustomer = underTest.findById(id);
+        assertThat(optionalCustomer)
+                .isPresent()
+                .hasValueSatisfying(c -> {
+//                    assertThat(c.getId()).isEqualTo(id);
+//                    assertThat(c.getName()).isEqualTo(name);
+//                    assertThat(c.getPhoneNumber()).isEqualTo(phoneNumber);
+                    assertThat(c).isEqualToComparingFieldByField(customer);
+                });
+    }
+}
