dc632e395cf10cdd0d37f2f2ac9adc18683d533f
Davide Di Giovanni <davidedigio@yahoo.it>
2022-12-04 15:23:25 +0100

    Added test cases.

--- src/test/java/com/davidedigiovanni/testing/customer/CustomerRepositoryTest.java
+++ src/test/java/com/davidedigiovanni/testing/customer/CustomerRepositoryTest.java
@@ -1,17 +1,21 @@
 package com.davidedigiovanni.testing.customer;
 
-import com.davidedigiovanni.testing.customer.Customer;
-import com.davidedigiovanni.testing.customer.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@DataJpaTest
+@DataJpaTest(
+        properties = {
+                "spring.jpa.properties.javax.persistence.validation.mode=none"
+        }
+)
 class CustomerRepositoryTest {
 
     @Autowired
@@ -38,5 +42,20 @@ class CustomerRepositoryTest {
 //                    assertThat(c.getPhoneNumber()).isEqualTo(phoneNumber);
                     assertThat(c).isEqualToComparingFieldByField(customer);
                 });
+    }
+
+    @Test
+    void itShouldNotSaveCustomerWhenNameIsNull() {
+        // Given
+        UUID id = UUID.randomUUID();
+        String name = "Dave";
+        String phoneNumber = "0123456789";
+        Customer customer = new Customer(id, null, phoneNumber);
+
+        // When
+        // Then
+        assertThatThrownBy(() -> underTest.save(customer))
+                .isInstanceOf(DataIntegrityViolationException.class)
+                .hasMessageContaining("not-null property references a null or transient value : com.davidedigiovanni.testing.customer.Customer.name");
     }
 }
